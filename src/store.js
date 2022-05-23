e0ab935d023356e45819192b4b5500f64918b817
Johannes Hofmockel <johannes.hofmockel@lavita.de>
2022-05-23 09:22:06 +0200

    change store functions

--- src/store.js
+++ src/store.js
@@ -1,3 +1,18 @@
 import { AbstractStore } from "@lavita-io/bizberry-sdk"
 
-export class BrowserStore extends AbstractStore {}
+export class BrowserStore extends AbstractStore {
+    async set(key, value, options = {}) {
+        if (options.isPersistent) document.cookie = `${this.cookie_prefix}_${key}=${value};`
+        return await super.set(key, value, options)
+    }
+
+    async get(key) {
+        const value = await super.get(key)
+
+        if (value === undefined) {
+            return
+        }
+
+        return value
+    }
+}
